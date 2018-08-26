cb02da0797d6d9881b67e0ad860c572ce04b3a78
Michael Lang <michael.lang@nexul.com>
2018-08-26 16:56:41 -0500

    Step011: ask user for player names

--- TicTacToe.ConsoleApp/Program.cs
+++ TicTacToe.ConsoleApp/Program.cs
@@ -17,7 +17,7 @@ namespace TicTacToe.ConsoleApp
                 Console.WriteLine(string.Format("{0}:{1}", cell.Row, cell.Col));
             }
 
-            // TODO: ask for player names
+            SetupPlayers(board);
 
             Console.WriteLine("Press ENTER to begin");
             Console.ReadLine();
@@ -30,6 +30,24 @@ namespace TicTacToe.ConsoleApp
             var board = new GameBoard();
             board.Generate(rows, cols);
             return board;
+        }
+
+        public static void SetupPlayers(GameBoard board)
+        {
+            var count = PromptInteger("How many players?");
+            for (int p = 0; p < count; p++)
+            {
+                Console.Write(string.Format("Name of player {0}: ", p + 1));
+                var name = Console.ReadLine();
+                var marker = "";
+                while (string.IsNullOrWhiteSpace(marker))
+                {
+                    Console.Write(name + "'s marker: ");
+                    marker = (Console.ReadKey().KeyChar).ToString();
+                }
+                Console.WriteLine();
+                board.Players.Add(new Player { Name = name, Marker = marker });
+            }
         }
 
         private static int PromptInteger(string message)
