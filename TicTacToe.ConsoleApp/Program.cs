1515bd65b284fad8a693ddb1edb20629552c838a
Michael Lang <michael.lang@nexul.com>
2018-08-26 16:31:48 -0500

    Step005: create the game board using a static method

--- TicTacToe.ConsoleApp/Program.cs
+++ TicTacToe.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TicTacToe.ConsoleApp.Models;
 
 namespace TicTacToe.ConsoleApp
 {
@@ -8,9 +9,28 @@ namespace TicTacToe.ConsoleApp
         {
             Console.WriteLine("Tic Tac Toe");
 
+            var board = CreateGameBoard();
+            // TODO: render the board
 
             Console.WriteLine("Press ENTER to begin");
             Console.ReadLine();
+        }
+        public static GameBoard CreateGameBoard()
+        {
+            var board = new GameBoard();
+            board.Grid = new Grid();
+
+            for (int r = 0; r < 3; r++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    var cell = new GridCell();
+                    cell.Row = r;
+                    cell.Col = c;
+                    board.Grid.Cells.Add(cell);
+                }
+            }
+            return board;
         }
     }
 }
